474f2ae8c8d5129f5b4bb764eb83349348a35e58
Maximiliano Minutillo <maximilminu@gmail.com>
2022-10-21 10:59:05 -0300

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { MoviesContext } from '../contexts/moviesContext'
 import Hero from './Hero'
 import Slider from "./Slider"
 
+
 const Home = () => {
 
   const { popularMovies, topRatedMovies, blockbusterMovies } = useContext(MoviesContext)
